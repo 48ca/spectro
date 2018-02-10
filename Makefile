8d9aec620938601ccb72114124ba6e0e6938b6b4
James Houghton <jamesthoughton@gmail.com>
2018-02-10 00:09:27 -0500

    CMake

--- Makefile
+++
@@ -1,7 +0,0 @@
-.PHONY:
-	clean
-
-CXXFLAGS=-O3 -Wall -Wextra
-
-spectro: spectro.cpp
-	$(CXX) $(CXXFLAGS) -o spectro spectro.cpp -lsndfile
--- spectro.cpp
+++ spectro.cpp
@@ -1,9 +1,12 @@
 #include <string>
 #include <iostream>
+#include <vector>
 
 #include <stdlib.h>
-
 #include <sndfile.hh>
+
+#include <fftw3.h>
+#include <SDL2/SDL_mixer.h>
 
 struct arguments {
     std::string filename;
@@ -18,49 +21,52 @@ class Sound { // libsndfile abstraction
     void open(void) {
         std::cout << "Opening file..." << std::endl;
         file = sf_open(filename.c_str(), SFM_READ, &info);
+        if(!file) failedToOpen = true;
+        music = Mix_LoadMUS(filename.c_str());
+        if(!music) failedToOpen = true;
     }
 
     void fillBuffer(void) {
-        if(buffer) {
+        if(!buffer.empty()) {
             std::cerr << "Buffer already full?" << std::endl;
-            return;
         }
-        std::cout << "Allocating..." << std::endl;
-        buffer = (double*) malloc(sizeof(double) * info.frames * info.channels);
-        std::cout << "Reading " << info.frames << " frames..." << std::endl;
-        sf_count_t read = sf_readf_double(file, buffer, info.frames);
-        std::cout << "Read " << read << " frames" << std::endl;
+        buffer.reserve(info.frames * info.channels);
+        /*sf_count_t read = */
+        sf_readf_double(file, &buffer[0], info.frames);
     }
 
   public:
+    Mix_Music *music;
     std::string filename;
     SF_INFO info;
-    double* buffer = nullptr;
+    std::vector<double> buffer;
+    bool failedToOpen = false;
 
     void displayInfo(void) const {
-        std::cout << "/***** SOUND FILE *****/" << '\n'
-                  << " * Filename: " << filename << '\n'
+        std::cout << "/***** " << filename << " *****\\" << '\n'
                   << " * Channels: " << info.channels << '\n'
                   << " * Frames: " << info.frames << '\n'
                   << " * Sample rate: " << info.samplerate << '\n'
-                  << "/**********************/" << std::endl;
+                  << "\\******" << std::string(filename.length(), '*') << "******/" << std::endl;
     }
 
     void read(void) {
-        fillBuffer();
+        if(file)
+            fillBuffer();
     }
 
     Sound(std::string fn) : file(nullptr), filename(fn) {
         open();
     }
-    ~Sound() noexcept {
+    ~Sound() {
         if(file != nullptr)
             sf_close(file);
-        if(buffer != nullptr) {
-            free(buffer);
-        }
     }
 };
+
+void displayFFT(const std::vector<double>& buffer) {
+
+}
 
 struct arguments parseArgs(int argc, char** argv) {
     struct arguments args;
@@ -76,7 +82,30 @@ struct arguments parseArgs(int argc, char** argv) {
     }
     if(args.filename.empty()) args.exitCode = 1;
     return args;
-};
+}
+
+void display(const Sound& sound) {
+    if(sound.info.channels != 2) {
+        std::cout << "Stereo input only please" << std::endl;
+        return;
+    }
+    std::cout << "Displaying..." << std::endl;
+
+    const long sample_size = 44100;
+    unsigned long max = sound.info.frames * sound.info.channels;
+    unsigned long left = 0;
+    unsigned long right = 1;
+    for(;
+            left < max && right < max;
+            left += sample_size * 2, right += sample_size * 2) {
+    }
+}
+
+void play(const Sound& sound) {
+    if(!sound.music) {
+        std::cerr << "Failed to play music with SDL" << std::endl;
+    }
+}
 
 int main(int argc, char** argv) {
     auto args = parseArgs(argc, argv);
@@ -89,11 +118,13 @@ int main(int argc, char** argv) {
 
     {
         Sound snd(args.filename);
-        if(!snd.info.frames)
+        if(snd.failedToOpen)
             std::cout << "Failed to open the file" << std::endl;
         else {
             snd.displayInfo();
             snd.read();
+            play(snd);
+            display(snd);
         }
     }
 
